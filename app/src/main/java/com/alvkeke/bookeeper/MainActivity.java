4ad9391bde31b06acfa11cb49e49af5bc42206b1
alvkeke@126.com <alvkeke@126.com>
2023-03-18 13:16:04 +0800

    [ui/book] add Activity to create new book item.

--- app/src/main/java/com/alvkeke/bookeeper/MainActivity.java
+++ app/src/main/java/com/alvkeke/bookeeper/MainActivity.java
@@ -1,7 +1,11 @@
 package com.alvkeke.bookeeper;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -9,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.alvkeke.bookeeper.data.BookItem;
+import com.alvkeke.bookeeper.ui.ItemAddActivity;
 import com.alvkeke.bookeeper.ui.booklist.BookListAdapter;
 
 import java.util.ArrayList;
@@ -16,6 +21,7 @@ import java.util.Date;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
 
 
     private void randomFillBook(ArrayList<BookItem> bookItems) {
@@ -34,20 +40,35 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    static class BtnAddItemClickListener implements View.OnClickListener {
+
+        private final Context ctx;
+        public BtnAddItemClickListener(Context ctx) {
+            this.ctx = ctx;
+        }
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(ctx, ItemAddActivity.class);
+            ctx.startActivity(intent);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<BookItem> bookItems = new ArrayList<>();
-
-        BookListAdapter adapter = new BookListAdapter(bookItems);
         RecyclerView bookList = findViewById(R.id.list_book_list);
+        Button btnAddItem = findViewById(R.id.btn_add_item);
 
+        btnAddItem.setOnClickListener(new BtnAddItemClickListener(this));
+
+        ArrayList<BookItem> bookItems = new ArrayList<>();
+        BookListAdapter adapter = new BookListAdapter(bookItems);
         bookList.setAdapter(adapter);
         bookList.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
         bookList.setLayoutManager(new LinearLayoutManager(this));
-
         randomFillBook(bookItems);
 
     }
---
+++ app/src/main/java/com/alvkeke/bookeeper/ui/ItemAddActivity.java
@@ -0,0 +1,16 @@
+package com.alvkeke.bookeeper.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alvkeke.bookeeper.R;
+
+public class ItemAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_add);
+    }
+}
